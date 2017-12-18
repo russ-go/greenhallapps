dbfb6986a77b3bf3fdbcb8aad34b8e058a5617ea
Russell Green Osborne <russell@greenhall.ca>
2017-12-18 08:48:41 -0800

    Original 2016 Greenhall.ca Web Site and Assets

---
+++ public_html/assets/contactform/thanks.php
@@ -0,0 +1,4 @@
+<h1>Thanks!</h1>
+<p> Your request has been processed.  Someone will be in contact with you shortly.  Thank for you interest, Greenhall</p>
+<p>You will be redirected shortly or <a href="../index.html">return to greenhall.ca</a></p>
+
